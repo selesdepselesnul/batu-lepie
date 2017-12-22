cf6fabdd9f3ed2dd3f240b7a2ae7bc27adc4627c
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-22 18:00:10 +0700

    add readBattery func

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+type Battery struct {
+	capacity int
+	status string
+}
+
+func readBattery() (*Battery, error) {
+	const batteryPath = "/sys/class/power_supply/BAT1/"
+	
+	capacity, errCapacity := ioutil.ReadFile(batteryPath + "capacity")
+	status, errStatus := ioutil.ReadFile(batteryPath + "status")
+	
+	if errCapacity != nil || errStatus != nil {
+		if errCapacity != nil {
+			return nil, errCapacity
+		} else {
+			return nil, errStatus
+		}
+	}
+
+	capacityInt, errCapacityParsed := strconv.Atoi(strings.TrimSuffix(string(capacity), "\n"))
+
+	if errCapacityParsed != nil {
+		return nil, errCapacityParsed
+	}
+	
+	return &Battery{capacity: capacityInt, status: string(status)}, nil
+}
 
 func main() {
-	fmt.Println("batu-lepie");
+	fmt.Println(readBattery())
 }
+
+
+
+
+
